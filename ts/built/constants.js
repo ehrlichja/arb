735d35a3a3c5bad7002f5a13b1935c6b5a719106
Andrew Ehrlich <andrew@aehrlich.com>
2017-11-28 18:16:02 -0800

    rm built

--- ts/built/constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ARKBTC = "ARKBTC";
-exports.VTCBTC = "VTCBTC";
-exports.pairId = {
-    "ARK-BTC": exports.ARKBTC,
-    "BTC-VTC": exports.VTCBTC,
-    "vtc-btc": exports.VTCBTC
-};
--- ts/built/main.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var coingi = require("./exchanges/coingi");
-console.log(coingi.z);
