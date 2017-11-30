24a21ee728ec539c1c77be19beb12fcbe5aa0d32
Andrew Ehrlich <andrew@aehrlich.com>
2017-11-29 20:53:28 -0800

    more bot work

--- ts/src/bot.ts
+++ ts/src/bot.ts
@@ -5,23 +5,68 @@ import { Bittrex } from './exchanges/bittrex';
 import { Order } from "./order";
 import { setInterval } from 'timers';
 
-let orders: {} = {};
+import * as lokijs from 'lokijs';
 
-function join(): void {
-  Object.keys(orders).map(function(ts) {
-    Object.keys(orders[ts]).map(function(tradingPair) {
-      Object.keys(orders[ts][tradingPair]).map(function(exchange) {
-        console.log(`${ts} - ${tradingPair} - ${exchange} - ${orders[ts][tradingPair][exchange].length}`)
+let orders: {} = {};
+let db = new lokijs("orders");
+
+function analyzer(): void {
+  let a = db.addCollection("analysis");
+  console.log(orders);
+  a.insert(orders);
+  /*
+  let thisOrders = orders;
+  let x = {};
+  Object.keys(thisOrders).map(function(ts) {
+    Object.keys(thisOrders[ts]).map(function(tradingPair) {
+      Object.keys(thisOrders[ts][tradingPair]).map(function(exchange) {
+        Object.keys(thisOrders[ts][tradingPair][exchange]).map(function(orderType) {
+          if (x[tradingPair] == undefined) {
+            x[tradingPair] = {};
+          }
+          if (x[tradingPair][exchange] == undefined) {
+            x[tradingPair][exchange] = [];
+          }
+          if (x[tradingPair][exchange][orderType] == undefined) {
+            x[tradingPair][exchange][orderType] = -1;
+          }
+          let prices = thisOrders[ts][tradingPair][exchange][orderType].map(function(order) {
+            return order.price;
+          }).sort()
+          let price = -1;
+          if (orderType == "SELL") {
+            price = prices[0];
+          } else {
+            price = prices[prices.length - 1];
+          }
+          x[tradingPair][exchange][orderType] = price;
+        });
       })
     });
   });
+
+
+  function f(a: string, b: string): string { return a; }
+  Object.keys(x).map(function(tradingPair) {
+    Object.keys(x[tradingPair]).reduce(f);
+  });
+*/
+  let x = a.findOne({ price: { '$gte': 0 } })
+  db.removeCollection("analysis");
+  console.log(x);
+  console.log("------------------------------------------------")
 }
 
 function summary(): void {
   Object.keys(orders).map(function(ts) {
     Object.keys(orders[ts]).map(function(tradingPair) {
       Object.keys(orders[ts][tradingPair]).map(function(exchange) {
-        console.log(`${ts} - ${tradingPair} - ${exchange} - ${orders[ts][tradingPair][exchange].length} orders`)
+        Object.keys(orders[ts][tradingPair][exchange]).map(function(orderType) {
+          let prices = orders[ts][tradingPair][exchange][orderType].map(function(order){
+            return order.price;
+          }).sort();
+          console.log(`${ts} - ${tradingPair} - ${exchange} - ${orderType} ${orders[ts][tradingPair][exchange][orderType].length} orders - ${prices[0]} -  ${prices[prices.length - 1]}`)
+        });
       })
     });
   });
@@ -34,6 +79,7 @@ function handler(order: Order): void {
   let ts = order.ts.getTime();
   let tradingPair = order.tradingPair;
   let exchange = order.exchange;
+  let orderType = order.orderType;
   if (orders[ts] == undefined) {
     orders[ts] = {};
   }
@@ -41,9 +87,12 @@ function handler(order: Order): void {
       orders[ts][tradingPair] = {};
   }
   if (orders[ts][tradingPair][exchange] == undefined) {
-    orders[ts][tradingPair][exchange] = [];
+    orders[ts][tradingPair][exchange] = {};
   }
-  orders[ts][tradingPair][exchange].push(order);
+  if (orders[ts][tradingPair][exchange][orderType] == undefined) {
+    orders[ts][tradingPair][exchange][orderType] = [];
+  }
+  orders[ts][tradingPair][exchange][orderType].push(order);
   if (len > 20) {
     delete orders[Object.keys(orders)[0]];
   }
@@ -55,6 +104,6 @@ new Bittrex().open("BTC-VTC", handler);
 new Bittrex().open("BTC-LTC", handler);
 
 setInterval(
-  summary,
+  analyzer,
   1000
 );
