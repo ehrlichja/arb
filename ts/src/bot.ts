bbce12351038d4b63a40254d6e135a9ada2995ca
Andrew Ehrlich <andrew@aehrlich.com>
2017-11-30 11:16:01 -0800

    alasql all the way

--- ts/src/bot.ts
+++ ts/src/bot.ts
@@ -5,36 +5,12 @@ import { Bittrex } from './exchanges/bittrex';
 import { Order } from "./order";
 import { setInterval } from 'timers';
 
-import * as _ from 'underscore'
 import * as alasql from 'alasql'
 
 let orders = [];
 
 function analyzer(): void {
   let thisOrders = orders;
-  function reducer(acc, order, i, arr) {
-    if (acc[order.tradingPair] == undefined) {
-      acc[order.tradingPair] = {};
-    }
-    if (acc[order.tradingPair][order.orderType] == undefined) {
-      acc[order.tradingPair][order.orderType] = {};
-    }
-    if (acc[order.tradingPair][order.orderType][order.exchange] == undefined) {
-      acc[order.tradingPair][order.orderType][order.exchange] = [];
-    }
-    acc[order.tradingPair][order.orderType][order.exchange].push(order.price);
-    return acc;
-  }
-  let reduced = thisOrders.reduce(reducer, {});
-  _.mapObject(reduced, function(orderTypes, tradingPair) {
-    _.mapObject(orderTypes, function(orders, buyOrSell){
-        _.mapObject(orders, function(prices, exchange) {
-          let ps = prices.sort()
-          let p = buyOrSell == "BUY" ? ps[0] : ps[ps.length - 1];
-          // console.log(`${tradingPair} - ${buyOrSell} - ${exchange} - ${p} `)
-        })
-    })
-  })
   let query = `
     SELECT 
     a.tradingPair, 
@@ -44,11 +20,15 @@ function analyzer(): void {
     b.orderType AS b_o, 
     a.price AS a_price, 
     b.price AS b_price,
-    a.price - b.price AS profit
+    IF(a.orderType = "BUY", a.price - b.price, b.price - a.price) AS profit
     FROM ? a 
-    JOIN ? b ON a.exchange != b.exchange 
+    JOIN ? b 
+    ON a.exchange != b.exchange 
     AND a.tradingPair = b.tradingPair 
     AND a.orderType != b.orderType
+    WHERE IF(a.orderType = "BUY", a.price - b.price, b.price - a.price) > 0
+    ORDER BY profit DESC
+    LIMIT 3
   `
   console.log(alasql(query, [thisOrders, thisOrders]));
   console.log("------------------------------------------------");
